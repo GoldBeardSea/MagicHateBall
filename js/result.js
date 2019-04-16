c60d2ad576aff2fbe4f3c04db65c2d87b9f48354
Will Fritts <waf515@gmail.com>
2019-04-16 14:31:57 -0700

    displays correct random word

--- js/result.js
+++ js/result.js
@@ -1,8 +1,18 @@
 'use strict';
-let anagramArray = ['i', 'e', 'g', 'h', 't'];
 let userObjArray = JSON.parse(localStorage.getItem('endState'));
 let userQuery = document.getElementById('anagramQuery');
 
+function scrambleWord(word){
+  var unscrambled = word.split('');
+  for(var i = word.length - 1; i > 0; i--){
+    var scramble = Math.floor(Math.random() * (i + 1));
+    var temp = unscrambled[i];
+    unscrambled[i] = unscrambled[scramble];
+    unscrambled[scramble] = temp;
+  }
+  var scrambled = unscrambled.join('');
+  return scrambled;
+}
 
 
 
@@ -15,7 +25,8 @@ function renderResultsList() {
     ulEl.appendChild(liEl);
   }
   let pEl = document.getElementById('anagramResult');
-  pEl.textContent = anagramArray.join('');
+  var scrambledKey = scrambleWord(userObjArray[0].key);
+  pEl.textContent = scrambledKey;
 } renderResultsList();
 
 let anagramQuery = function (event) {
