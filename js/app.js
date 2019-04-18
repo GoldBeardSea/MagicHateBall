c93c411fc46f42b8837bbfafa611267388ded7d5
Timothy Joseph Busch <timbusch1@gmail.com>
2019-04-18 14:42:02 -0700

    needed to save on appjs

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ var thisRoundMagicWord;
 var responseArray = [];
 var magicWords = ['eight', 'hate','irate','angry','annoyance', 'spite','insult','injury'];
 let shake = new Audio('../audio/shake.wav');
+shake.volume = .5;
 
 function User(name, key) {
   this.name = name;
